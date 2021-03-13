d7679b5f044e0255193dfa506df8c4233fa70ab4
SariMelina <mmkings_beba@hotmail.com>
2021-03-13 09:21:37 -0600

    Solution by Sara

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ function twoNumberSum(array, targetSum) {
 		sum += data
 	}
 	
-  // Write your code here.
+	// Write your code here.
+	for(let i=0; i<array.length;i++){
+    for(let j=i+1; j<array.length;j++){
+      if(array[i]+array[j] === targetSum){
+        return x = [array[i], array[j]]
+      }
+    }
+  }
+  return x = []
 }
 
 // Do not edit the line below.
